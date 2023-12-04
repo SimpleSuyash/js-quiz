29edd55bef33038e1935d246f15281524fb985b9
SimpleSuyash <suyashmaharjan@gmail.com>
2023-12-05 05:39:30 +1100

    index page update

--- src/script/index.js
+++ src/script/index.js
@@ -8,53 +8,53 @@ const q2 = {
     question: "The condition in an if/else statement is enclosed with _____.",
     options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
     answer: "curly brackets",
-}
+};
 const q3 = {
     question: "Always in JavaScript can be used to store _____.",
     options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     answer: "all of the above",
-}
+};
 const q4 = {
     question: "String valuse must be enclosed within _____ when being assigned to variables.",
     options: ["commas", "curly brackets", "quotes", "parenthesis"],
     answer: "quotes",
-}
+};
 const q5 = {
     question: "A very useful tool used during development and debugging for printing content to the dubugger is:",
     options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
     answer: "console.log",
-}
+};
 
 const questions = [q1, q2, q3, q4, q5];
-let currentQuestionNum = 0;
+const instructionSection = get(instruction);
+const questionSection = get(question);
+const resultSection = get(result);
+const tallySection = get(tally);
+const timer = get(time);
 
-
+window.onload = function (){
+    questionSection.style.display = "none";
+    resultSection.style.display = "none";
+    tallySection.style.display = "none";
+};
 
 function startQuiz() {
+   
+    instructionSection.style.display = "none";
+    countdown();
     showQuestions();
 }
 
-function showQuestions() {
-    let paragraph, orderList, list;
-    instructionSection = document.querySelector(".instruction");
-    instructionSection.innerText = "";
-    questionSection = document.querySelector(".question");
-    paragraph = document.createElement("p");
 
-    for (let i = 0; i < questions.length; i++) {
-        if (i == currentQuestionNum) {
-            paragraph.innerText = questions[i].question;
-            questionSection.innerText = "";
-            questionSection.appendChild(paragraph);
 
-            for (let j in questions[i].options) {
-                let button = document.createElement("a");
-                button.innerText = (++j) + "." + questions[i].options[--j];
-                questionSection.appendChild(button);
-            }
-            break;
-        }
+
+function create(createMe, type ="element"){
+    if(type == "node"){
+        return document.createTextNode("createMe");
+    }else{
+        return document.createElement("createMe");
     }
-
 }
-
+function get(id){
+    return document.getElementById("id");
+}
